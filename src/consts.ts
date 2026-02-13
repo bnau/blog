bc6d9a19bc44eb790bc0426b962ace88fbca2c48
Bertrand Nau <naubertrand@gmail.com>
2026-02-13 15:51:57 +0100

    fix: email

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ export const SITE_TITLE = 'Bertrand Nau';
 export const SITE_DESCRIPTION = 'Blog de Bertrand Nau';
 
 export const SOCIAL_PROFILES = {
-  email: 'naubertrand@gmail.com',
+  email: 'contact@bertrand-nau.fr',
   github: 'bnau',
   linkedin: 'bertrand-nau',
   bluesky: 'bertrandnau.bsky.social',
